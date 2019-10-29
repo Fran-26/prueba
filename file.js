7460dccd1a46cc64243d2e67616582ed9f4fee56
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2019-10-29 08:10:29 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+	setInterval (update, 3000);
+});
+
+function update (){
 	var reqURL = 'https://io.adafruit.com/api/v2/veintiseis/groups/sensores?x-aio-key=5515a3ad171c4034afe4ec86ed34999f'
 	var req = new XMLHttpRequest();
 	req.open('GET', reqURL, true);
 	req.onreadystatechange = function () {
-		if (req.readyState == 4)
-  		if  (req.status == 200) {
-   			obj = JSON.parse(req.responseText);
-				sensor_1.innerHTML += obj.feeds[0].last_value;
-				sensor_2.innerHTML += obj.feeds[1].last_value;
-				sensor_3.innerHTML += obj.feeds[2].last_value;
-			
-  		}
-			else {
-	   html.innerHTML+="<p>error "+req.status;
- 			}
-		};
-req.send(null);
-});
+	if (req.readyState == 4)
+		if  (req.status == 200) {
+   		obj = JSON.parse(req.responseText);
+			sensor_1.innerHTML += obj.feeds[0].last_value;
+			sensor_2.innerHTML += obj.feeds[1].last_value;
+			sensor_3.innerHTML += obj.feeds[2].last_value;
+		}
+		else {
+	  	html.innerHTML+="<p>error "+req.status;
+ 		}
+	};
+	req.send(null);
+}
