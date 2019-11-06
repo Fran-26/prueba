0700a3633cbbc996af9e985cbf64a44afb5f1195
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2019-11-06 13:36:14 -0300

    Update file.js

--- file.js
+++ file.js
@@ -9,21 +9,23 @@ function update (){
 	req.onreadystatechange = function () {
 	if (req.readyState == 4)
 		if  (req.status == 200) {
-   			obj = JSON.parse(req.responseText);
-			obj.feeds.forEach() => {
-				if (this.name = "sensor_1")
-					sensor_1.innerHTML = "Puerta Principal: " + this.last_value;
-				if (this.name = "sensor_2")
-					sensor_2.innerHTML = "Ventana Sala: " + this.last_value;
-				if (this.name = "sensor_3")
-					sensor_3.innerHTML = "Ventana Atras: " + this.last_value;
-				if (this.name = "temperature")
-					sensor_4.innerHTML = "Temperatura de la Sala: " + this.last_value;				
-			}
+			obj = JSON.parse(req.responseText);
+			obj.feeds.forEach(myFunction)
 		}
 		else {
 	  	html.innerHTML+="<p>error "+req.status;
  		}
 	};
 	req.send(null);
+}
+
+function myFunction (item) {
+	if (item.name == "sensor_1")
+		sensor_1.innerHTML = "Puerta Principal: " + item.last_value;
+	if (item.name == "sensor_2")
+		sensor_2.innerHTML = "Ventana Sala: " + item.last_value;
+	if (item.name == "sensor_3")
+		sensor_3.innerHTML = "Ventana Atras: " + item.last_value;
+	if (item.name == "temperature")
+		sensor_4.innerHTML = "Temperatura de la Sala: " + item.last_value;				
 }
