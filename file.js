0bedf415bf9a9b32d7652fd0edf6856ad153e10d
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2019-11-09 10:50:50 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,31 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
-	setInterval (update, 3000);
+	setInterval (update, 3000), setInterval(alarma, 10000)
 });
 
+var armado = 0; 
+var puerta_1, ventana_1, ventana_2, temperatura_1;
+
 function update (){
-	var reqURL = '	https://io.adafruit.com/api/v2/veintiseis/groups/default?x-aio-key=5515a3ad171c4034afe4ec86ed34999f'
+	var reqURL = 'https://io.adafruit.com/api/v2/veintiseis/groups/default?x-aio-key=5515a3ad171c4034afe4ec86ed34999f'
 	var req = new XMLHttpRequest();
 	req.open('GET', reqURL, true);
 	req.onreadystatechange = function () {
 	if (req.readyState == 4)
 		if  (req.status == 200) {
 			obj = JSON.parse(req.responseText);
-			obj.feeds.forEach(myFunction)
+			obj.feeds.forEach((item) => {
+				if (item.name == "command") {
+					if ((item.last_value === "1") && (armado === 0)){
+						connect.innerHTML = "Desbloquear Alarma <i class=\"fas fa-lock-open\"></i>";
+						armado = 1;
+					}
+					if ((item.last_value === "0") && (armado === 1)){
+						connect.innerHTML = "Bloquear Alarma <i class=\"fas fa-lock\"></i>";
+						armado = 0;
+					}
+				}
+				if (item.name == "sensor_1") {
+					sensor_1.innerHTML = "Puerta Principal: " + item.last_value;
+					puerta_1 = item.last_value;
+				}
+				if (item.name == "sensor_2") {
+					sensor_2.innerHTML = "Ventana Sala: " + item.last_value;
+					ventana_1 = item.last_value;
+				}
+				if (item.name == "sensor_3") {
+					sensor_3.innerHTML = "Ventana Atras: " + item.last_value;
+					ventana_2 = item.last_value;
+				}
+				if (item.name == "temperature") {
+					sensor_4.innerHTML = "Temperatura de la Sala: " + item.last_value;								
+					temperatura_1 = item.last_value;
+				}
+			});
 		}
 		else {
 	  	html.innerHTML+="<p>error "+req.status;
  		}
 	};
 	req.send(null);
+
 }
 
-function myFunction (item) {
-	if (item.name == "sensor_1")
-		sensor_1.innerHTML = "Puerta Principal: " + item.last_value;
-	if (item.name == "sensor_2")
-		sensor_2.innerHTML = "Ventana Sala: " + item.last_value;
-	if (item.name == "sensor_3")
-		sensor_3.innerHTML = "Ventana Atras: " + item.last_value;
-	if (item.name == "temperature")
-		sensor_4.innerHTML = "Temperatura de la Sala: " + item.last_value;				
+function alarma () {
+	if (armado && (puerta_1 == "abierta" || ventana_1 == "abierta" || ventana_2 == "abierta")) {
+		alert("Zona abierta");
+	}
+}
+
+function activar (){
+	let xhr = new XMLHttpRequest();
+	let url = "https://io.adafruit.com/api/v2/veintiseis/feeds/command/data.json?X-AIO-Key=5515a3ad171c4034afe4ec86ed34999f";
+	
+	xhr.open("POST", url, true);
+	
+	xhr.setRequestHeader("Content-Type", "application/json");
+
+	xhr.onreadystatechange = function () { 
+		if (xhr.readyState === 4 && xhr.status === 200) { 
+			obj = JSON.parse(this.responseText);
+			if (obj.feed_key == "command") {
+				if ((obj.value === "1") && (armado === 0)){
+					connect.innerHTML = "Desbloquear Alarma <i class=\"fas fa-lock-open\"></i>";
+					armado = 1;
+				}
+				if ((obj.value === "0") && (armado === 1)){
+					connect.innerHTML = "Bloquear Alarma <i class=\"fas fa-lock\"></i>";
+					armado = 0;
+				}
+			}
+		}
+	}; 	
+	var data = JSON.stringify({"value": ((armado+1)%2)})
+	xhr.send(data);
 }
